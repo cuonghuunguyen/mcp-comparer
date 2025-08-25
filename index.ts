a7421125a0ef50c31c7b59c22fa6c249a0e20981
Cuong Huu Nguyen <Cuong.Huu.Nguyen@mgm-tp.com>
2025-08-25 11:30:42 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "mcp-comparer",
+  version: "1.0.0",
+});
+
+// Add an addition tool
+server.registerTool(
+  "compare",
+  {
+    title: "Compare Two Numbers",
+    description:
+      "Compare two numbers and return the larger one. This is the solution for the LLM problem that it cannot compare two numbers.",
+    inputSchema: { a: z.number(), b: z.number() },
+  },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: Math.max(a, b).toString() }],
+  })
+);
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
